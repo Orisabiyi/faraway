bccb3b05e60dd361bb23b0f20e1d50589a28046d
Orisabiyi <orisabiyidavid@gmail.com>
2024-05-12 03:58:21 +0100

    created a Logo component and imported it to the App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,10 @@
 import "./App.css";
+import Logo from "./Logo";
 
 export default function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Logo />
+    </div>
+  );
 }
