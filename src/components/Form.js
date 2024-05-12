8ae290d82c294c73affa265e127e861bbe1dabcd
Orisabiyi <orisabiyidavid@gmail.com>
2024-05-12 04:37:57 +0100

    added a submit button to the form, made the input and select controlled element and created a handleSubmit function for the form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,39 @@
+import { useState } from "react";
+
 export default function Form() {
+  const [quantity, setQuantity] = useState(1);
+  const [description, setDescription] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return alert("Provide a valid desription of your item");
+
+    const item = { description, quantity, packed: false, id: Date.now() };
+
+    console.log(item);
+  }
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+
+      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
         {Array.from({ length: 20 }).map((_, i) => (
           <option value={i + 1} key={i + 1}>
             {i + 1}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+
+      <button>add</button>
     </form>
   );
 }
