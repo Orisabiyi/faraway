86a60687984497c0fc94273ee494d6128f32e314
Orisabiyi <orisabiyidavid@gmail.com>
2024-05-20 10:29:59 +0100

    created a bill splitter fr friends

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,78 @@
 import { useState } from "react";
-import "./App.css";
-import Form from "./Form";
-import Logo from "./Logo";
-import PackingList from "./PackingList";
-import Stats from "./Stats";
+import FriendList from "./FriendList";
+import FormAddFriend from "./FormAddFriend";
+import Button from "./Button";
+import FormSplitBill from "./FormSplitBill";
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+// 1. Create a static page layout of the web page using component
 
 export default function App() {
-  const [items, setItems] = useState([]);
+  const [friends, setFriends] = useState(initialFriends);
+  const [showFriend, setShowFriend] = useState(false);
+  const [curFriend, setCurFriend] = useState(null);
 
-  function handleToggleItem(id) {
-    setItems((items) =>
-      items.map((item) =>
-        item.id === id ? { ...item, packed: !item.packed } : item
-      )
+  function handleShowFriend() {
+    setShowFriend((show) => !show);
+  }
+
+  function handleSetFriend(friend) {
+    let newFriendExist = friends.filter(
+      (prevFriends) => friend.name === prevFriends.name
     );
+
+    if (newFriendExist.length) {
+      setShowFriend(false);
+      return alert(
+        `Provide a new friend name, ${newFriendExist[0].name} already exist`
+      );
+    }
+
+    setFriends((prevFriends) => [...prevFriends, friend]);
+    setShowFriend(false);
+  }
+
+  function handleCurFriend(friend) {
+    setCurFriend((curFriend) => (curFriend?.id === friend.id ? null : friend));
   }
 
   return (
     <div className="app">
-      <Logo />
-      <Form onItem={setItems} />
-      <PackingList items={items} onToggleItem={handleToggleItem} />
-      <Stats />
+      <div className="sidebar">
+        <FriendList
+          friends={friends}
+          curFriend={curFriend}
+          onCurFriend={handleCurFriend}
+        />
+
+        {showFriend && <FormAddFriend onSetFriend={handleSetFriend} />}
+
+        <Button onClick={handleShowFriend}>
+          {showFriend ? "close" : "Add Friend"}
+        </Button>
+      </div>
+
+      {curFriend && <FormSplitBill />}
     </div>
   );
 }
--- src/components/Stats.js
+++
@@ -1,7 +0,0 @@
-export default function Stats() {
-  return (
-    <footer className="stats">
-      <em>💼 You have X items on your list, and you already packed X (X%)</em>
-    </footer>
-  );
-}
