db7c40c54bfabdf4823a988eb6f799dffaab4573
Orisabiyi <orisabiyidavid@gmail.com>
2024-05-22 19:01:25 +0100

    styled the navigation bar of the app

---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+export default function Nav() {
+  return (
+    <nav className="nav">
+      <div className="nav__logo">
+        <div className="logo">T</div>
+        <h1>Trafalgar</h1>
+      </div>
+
+      <ul className="nav__list">
+        <li>Home</li>
+        <li>Find a doctor</li>
+        <li>Apps</li>
+        <li>Testimonials</li>
+        <li>About us</li>
+      </ul>
+    </nav>
+  );
+}
